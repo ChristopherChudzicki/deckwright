965d74a8a01ef6d71d4173aca80b8aa97c7a74c7
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-05-04 09:52:53 -0400

    test(api): add spell factories

--- src/api/factories.ts
+++ src/api/factories.ts
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { Factory } from "fishery";
-import type { MagicItem } from "../data/srd-schema";
+import type { MagicItem, Spell } from "../data/srd-schema";
 import type { MagicItemDetail, MagicItemIndex } from "./endpoints/magicItems";
+import type { SpellDetail, SpellIndex } from "./endpoints/spells";
 
 const rarities = ["Common", "Uncommon", "Rare", "Very Rare", "Legendary"];
 const categories = ["Wondrous Item", "Ring", "Rod", "Weapon", "Armor", "Potion", "Scroll", "Wand"];
@@ -39,5 +40,68 @@ export const magicItemIndexFactory = Factory.define<MagicItemIndex, MagicItemInd
 
 export const magicItemDetailFactory = Factory.define<MagicItemDetail>(() => ({
   ...magicItemIndexEntryFactory.build(),
+  ruleset: "2024",
+}));
+
+const spellSchools = [
+  "abjuration",
+  "conjuration",
+  "divination",
+  "enchantment",
+  "evocation",
+  "illusion",
+  "necromancy",
+  "transmutation",
+];
+
+const castingTimes = ["action", "bonus-action", "reaction", "minute", "hour"] as const;
+
+const spellClasses = [
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Paladin",
+  "Ranger",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+];
+
+export const spellIndexEntryFactory = Factory.define<Spell>(() => {
+  const slug = faker.helpers
+    .slugify(`${faker.lorem.words(2)}-${faker.string.alphanumeric(5)}`)
+    .toLowerCase();
+  return {
+    key: open5eKey(slug),
+    name: faker.lorem.words(2),
+    level: faker.number.int({ min: 0, max: 9 }),
+    school: { name: faker.helpers.arrayElement(spellSchools) },
+    casting_time: faker.helpers.arrayElement(castingTimes),
+    ritual: false,
+    range_text: "60 feet",
+    duration: "Instantaneous",
+    concentration: false,
+    verbal: true,
+    somatic: true,
+    material: false,
+    material_specified: "",
+    classes: [{ name: faker.helpers.arrayElement(spellClasses) }],
+    desc: faker.lorem.paragraph(),
+    higher_level: "",
+  };
+});
+
+type SpellIndexTransient = { size: number };
+
+export const spellIndexFactory = Factory.define<SpellIndex, SpellIndexTransient>(
+  ({ transientParams }) => {
+    const size = transientParams.size ?? 3;
+    const results = spellIndexEntryFactory.buildList(size);
+    return { count: results.length, results };
+  },
+);
+
+export const spellDetailFactory = Factory.define<SpellDetail>(() => ({
+  ...spellIndexEntryFactory.build(),
   ruleset: "2024",
 }));
