1b0f86aa062e735775f5490093e12149e83be7c8
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-05-09 11:15:03 -0400

    refactor(browse): add ContentType registry interfaces

---
+++ src/api/content-types/types.ts
@@ -0,0 +1,24 @@
+import type { Card } from "../../cards/types";
+import type { Ruleset } from "../endpoints/magicItems";
+
+export type ContentRow = {
+  key: string;
+  name: string;
+  meta: string;
+  toCard: () => Card;
+};
+
+export type ContentTypeResults = {
+  isLoading: boolean;
+  isError: boolean;
+  refetch: () => void;
+  rows: ReadonlyArray<ContentRow>;
+};
+
+export type ContentType = {
+  id: string;
+  label: string;
+  searchPlaceholder: string;
+  supportedSources: readonly Ruleset[];
+  useResults: (source: Ruleset, query: string) => ContentTypeResults;
+};
