668897cd9049c96376cbbb6ee0c87514cb98e457
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-05-09 11:19:54 -0400

    refactor(browse): add CONTENT_TYPES registry

---
+++ src/api/content-types/index.ts
@@ -0,0 +1,7 @@
+import { itemsContentType } from "./items";
+import { spellsContentType } from "./spells";
+import type { ContentType } from "./types";
+
+export const CONTENT_TYPES: readonly ContentType[] = [itemsContentType, spellsContentType];
+
+export type { ContentRow, ContentType, ContentTypeResults } from "./types";
