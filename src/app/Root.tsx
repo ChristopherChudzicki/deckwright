5f6982a7b4d898c77fcd79f5fe04a371b08880c2
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-05-09 20:53:40 -0400

    refactor(breadcrumb): hide single-segment crumb; add header spacer

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -14,6 +14,7 @@ export function Root() {
             D&amp;D Cards
           </Link>
           <DeckBreadcrumb />
+          <div className={styles.spacer} />
           <UserMenu />
         </header>
         <main className={styles.main}>
