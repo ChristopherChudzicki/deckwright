8fba96dacc406efa987da312fb24aac4945fce56
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-05-09 21:10:36 -0400

    feat(content-types): register mundane-items in registry

--- src/api/content-types/index.ts
+++ src/api/content-types/index.ts
@@ -1,9 +1,11 @@
 import { magicItemsContentType } from "./magic-items";
+import { mundaneItemsContentType } from "./mundane-items";
 import { spellsContentType } from "./spells";
 import type { ContentType } from "./types";
 
 export const CONTENT_TYPES: readonly [ContentType, ...ContentType[]] = [
   magicItemsContentType,
+  mundaneItemsContentType,
   spellsContentType,
 ];
 
